77a6cc322ab85ae6503f1c4b0673e6c1bbfe3b11
Igor Tronin <igortronin2705@gmail.com>
2022-03-06 14:59:22 +0400

    redux-toolkit-createSlice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+
+import {heroesFetching, heroesFetched, heroesFetchingError} from '../components/heroesList/heroesSlice'
+
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
     request("http://localhost:3001/heroes")
@@ -12,24 +15,6 @@ export const fetchFilters = (request) => (dispatch) => {
         .catch(() => dispatch(filtersFetchingError()))
 }
 
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
-
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
-
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
 
 export const filtersFetching = () => {
     return {
@@ -57,26 +42,5 @@ export const activeFilterChanged = (filter) => {
     }
 }
 
-// export const activeFilterChanged = (filter) => (dispatch) => {
-//     setTimeout(() => {
-//         dispatch({
-//             type: 'ACTIVE_FILTER_CHANGED',
-//             payload: filter
-//         })
-//     }, 1000)
-// }
 
-export const heroCreated = (hero) => {
-    return {
-        type: 'HERO_CREATED',
-        payload: hero
-    }
-}
-
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
-    }
-}
 
